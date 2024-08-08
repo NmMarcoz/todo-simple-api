e3f6898c166cfce137044e29187a16dcb7548745
nogueira <marcosrafael.nm62@gmail.com>
2024-08-07 21:44:19 -0300

    first commit

---
+++ src/main/java/com/lucasangelo/todosimple/TodosimpleApplication.java
@@ -0,0 +1,14 @@
+package com.lucasangelo.todosimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosimpleApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Aloooooooooo");
+		SpringApplication.run(TodosimpleApplication.class, args);
+	}
+
+}
